1f84eff0d4928079b25e2ec98d4c991cb1fe4470
skullbreakerr <96946235+skullbreakerr@users.noreply.github.com>
2023-06-01 18:20:30 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-// Import stylesheets
-import './style.css';
+function generateQR(){
+  var chartAPI="https://chart.googleapis.com/chart?cht=qr&chs=";
+  var inputext = document.getElementById("inputext").value;
+  var imgsize =document.getElementById("imgsize").value;
+  var qr=document.getElementById("img");
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+  if(inputext!=null && imgsize=='150'){
+     qr.src = chartAPI+"150x150&chl="+inputext;     
+  }else if(inputext!=null&&imgsize=='250'){
+      qr.src = chartAPI+"250x250&chl="+inputext;     
+  }else if(inputext!=null&&imgsize=='350'){
+     qr.src = chartAPI+"350x350&chl="+inputext;     
+  }else if(inputext!=null&&imgsize=='450'){
+     qr.src = chartAPI+"450x450&chl="+inputext;     
+  }else if(inputext!=null&&imgsize=='550'){
+     qr.src = chartAPI+"550x550&chl="+inputext;     
+  }else{
+      windows.alert("Empty text field");
+  }
+}
