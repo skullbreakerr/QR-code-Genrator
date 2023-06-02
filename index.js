cdd07de87e0bf2d37ee6b20f7f4a885da3be6884
skullbreakerr <96946235+skullbreakerr@users.noreply.github.com>
2023-06-02 13:36:14 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ const button = document.getElementById("generateQR");
 
 function generateQR(event){
   event.preventDefault();
-   var chartAPI="https://chart.googleapis.com/chart?cht=qr&chs=";
+   var chartAPI="https://chart.googleapis.com/chart?cht=qr&chs";
   var inputext = document.getElementById("inputext").value;
   var imgsize =document.getElementById("imgsize").value;
   var qr=document.getElementById("img");
-  
+  qr.src = chartAPI+"="+imgsize+"x"+imgsize+"&chl="+inputext;
 
-  if(imgsize=='150'){
-     qr.src = chartAPI+"150x150&chl="+inputext;     
-  }else if(imgsize=='250'){
-      qr.src = chartAPI+"250x250&chl="+inputext;     
-  }else if(imgsize=='350'){
-     qr.src = chartAPI+"350x350&chl="+inputext;     
-  }else if(imgsize=='450'){
-     qr.src = chartAPI+"450x450&chl="+inputext;     
-  }else if(imgsize=='550'){
-     qr.src = chartAPI+"550x550&chl="+inputext;     
-  }else{
-     qr.src = chartAPI+"250x250&chl=QRcode+Generator++Develop+by:+Dharmik+Patel";
-  }
+//   if(imgsize=='150'){
+//      qr.src = chartAPI+"150x150&chl="+inputext;     
+//   }else if(imgsize=='250'){
+//       qr.src = chartAPI+"250x250&chl="+inputext;     
+//   }else if(imgsize=='350'){
+//      qr.src = chartAPI+"350x350&chl="+inputext;     
+//   }else if(imgsize=='450'){
+//      qr.src = chartAPI+"450x450&chl="+inputext;     
+//   }else if(imgsize=='550'){
+//      qr.src = chartAPI+"550x550&chl="+inputext;     
+//   }else{
+//      qr.src = chartAPI+"250x250&chl=QRcode+Generator++Develop+by:+Dharmik+Patel";
+//   }
 }
 
 button.addEventListener('click',generateQR);
