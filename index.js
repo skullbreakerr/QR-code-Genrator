d888ff4e93543783369d9acc9140f581aab09f26
skullbreakerr <96946235+skullbreakerr@users.noreply.github.com>
2023-06-05 14:48:37 +0530

    5 files modified, 1 added

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 import './style.css';
-const button = document.getElementById("generateQR");
+const button = document.getElementById('generateQR');
 
-
-function generateQR(event){
+function generateQR(event) {
   event.preventDefault();
-   var chartAPI="https://chart.googleapis.com/chart?cht=qr&chs";
-  var inputext = document.getElementById("inputext").value;
-  var imgsize =document.getElementById("imgsize").value;
-  var qr=document.getElementById("img");
-  qr.src = chartAPI+"="+imgsize+"x"+imgsize+"&chl="+inputext;
-
-//   if(imgsize=='150'){
-//      qr.src = chartAPI+"150x150&chl="+inputext;     
-//   }else if(imgsize=='250'){
-//       qr.src = chartAPI+"250x250&chl="+inputext;     
-//   }else if(imgsize=='350'){
-//      qr.src = chartAPI+"350x350&chl="+inputext;     
-//   }else if(imgsize=='450'){
-//      qr.src = chartAPI+"450x450&chl="+inputext;     
-//   }else if(imgsize=='550'){
-//      qr.src = chartAPI+"550x550&chl="+inputext;     
-//   }else{
-//      qr.src = chartAPI+"250x250&chl=QRcode+Generator++Develop+by:+Dharmik+Patel";
-//   }
+  var chartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs';
+  var inputext = document.getElementById('inputext').value;
+  var imgsize = document.getElementById('imgsize').value;
+  var qr = document.getElementById('img');
+  if (inputext === '') {
+    qr.src =
+      chartAPI +
+      '=' +
+      imgsize +
+      'x' +
+      imgsize +
+      '&chl=QRcode+Generator++Develop+by:+Dharmik+Patel';
+  } else {
+    qr.src = chartAPI + '=' + imgsize + 'x' + imgsize + '&chl=' + inputext;
+  }
+  //   if(imgsize=='150'){
+  //      qr.src = chartAPI+"150x150&chl="+inputext;
+  //   }else if(imgsize=='250'){
+  //       qr.src = chartAPI+"250x250&chl="+inputext;
+  //   }else if(imgsize=='350'){
+  //      qr.src = chartAPI+"350x350&chl="+inputext;
+  //   }else if(imgsize=='450'){
+  //      qr.src = chartAPI+"450x450&chl="+inputext;
+  //   }else if(imgsize=='550'){
+  //      qr.src = chartAPI+"550x550&chl="+inputext;
+  //   }else{
+  //      qr.src = chartAPI+"250x250&chl=QRcode+Generator++Develop+by:+Dharmik+Patel";
+  //   }
 }
 
-button.addEventListener('click',generateQR);
+button.addEventListener('click', generateQR);
 
+// Head Text Effect//
 const textElement = document.getElementById('text');
 const words = ['QR Code Generator'];
 
@@ -58,3 +67,32 @@ function eraseText() {
 }
 
 typeText();
+
+// Accessing Camera for Scanning QR code 
+const config = {
+  inputStream: {
+    name: "Live",
+    type: "LiveStream",
+    target: "#camera",
+    constraints: {
+      facingMode: "environment", // Using the back camera
+    },
+  },
+  decoder: {
+    readers: ["qr_code_reader"], // Specify the desired barcode readers
+  },
+};
+
+Quagga.init(config, err => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  Quagga.start();
+});
+
+// Detecting QR code //
+Quagga.onDetected( result => {
+  const code = result.codeResult.code;
+  document.getElementById("result").value = code;
+});
